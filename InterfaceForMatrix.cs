fdb4a3ce6730561a0958c5fa8904e52beae9c8c2
Andrey1233-maker <andrey1mosckalenko@gmail.com>
2021-05-07 17:22:05 +0300

    Lab1

---
+++ InterfaceForMatrix.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Laba1
+{
+    class InterfaceForMatrix
+    {
+
+        public Matrix[] list;
+        public int count = -1;
+        public InterfaceForMatrix()
+        {
+            Console.Out.Write("Print count matrix : ");
+            count = this._ReaderInt();
+            list = new Matrix[count];
+            this.Main();
+        }
+
+        public void Main()
+        {
+            Console.Out.WriteLine("Choise command : ");
+            this._Info();
+
+            string command;
+
+            while (this._onWork)
+            {
+                Console.Out.Write(">>> ");
+                int numberCommand = this._ReaderInt();
+
+                switch (numberCommand)
+                {
+                    case 0: this._onWork = false;
+                        break;
+                    case 1: this._CreateMatrix();
+                        break;
+                    case 2:
+                        this._PrintMatrix();
+                        break;
+                    case 3:
+                        this._Info();
+                        break;
+                    case 4:
+                        this._Sum();
+                        break;
+                    case 5:
+                        this._Multy();
+                        break;
+                    case 6:
+                        this._Trans();
+                        break;
+                    case 7:
+                        this._PresentList();
+                        break;
+                }
+            }
+        }
+
+        private bool _onWork = true;
+
+        private void _Info()
+        {
+            Console.Out.WriteLine("1)Create new Matrix");
+            Console.Out.WriteLine("2)Print Matrix");
+            Console.Out.WriteLine("3)Present command list");
+            Console.Out.WriteLine("4)Sum pair of Matrix");
+            Console.Out.WriteLine("5)Multy pair of Matrix");
+            Console.Out.WriteLine("6)Transposition Matrix");
+            Console.Out.WriteLine("7)Presnt list of Matrix");
+            
+            Console.Out.WriteLine("0)Exit");
+        }
+
+        private void _CreateMatrix()
+        {
+            Console.Out.WriteLine("Print position in list: ");
+            int pos = this._ReaderInt();
+            Console.Out.WriteLine("Print size of new Matrix: ");
+            Console.Out.Write("Height: ");
+            int h = this._ReaderInt();
+            Console.Out.Write("Leight: ");
+            int l = this._ReaderInt();
+
+            list[pos] = this._ReadMatrix(h, l);
+        }
+
+        private void _PresentList()
+        {
+
+            for(int i = 0; i < this.count; i++)
+            {
+                if (list[i] != null)Console.Out.WriteLine(i + " - h = " + list[i].Row + " l = " + list[i].Colon);
+                else Console.Out.WriteLine(i + " - Empty");
+            }
+        }
+
+        private void _Sum()
+        {
+            Console.Out.WriteLine("Print position in list: ");
+            int pos = this._ReaderInt();
+            Console.Out.WriteLine("First Matrix: ");
+            int a1 = this._ReaderInt();
+            Console.Out.WriteLine("Second Matrix: ");
+            int a2 = this._ReaderInt();
+            if (this._MatrixIsCreate(a1) && this._MatrixIsCreate(a2))
+                list[pos] = list[a1] + list[a2];
+            else
+            {
+                Console.Out.WriteLine("Matrix isn't create!!!");
+            }
+        }
+
+        private void _Multy()
+        {
+            Console.Out.WriteLine("Print position in list: ");
+            int pos = this._ReaderInt();
+            Console.Out.WriteLine("First Matrix: ");
+            int a1 = this._ReaderInt();
+            Console.Out.WriteLine("Second Matrix: ");
+            int a2 = this._ReaderInt();
+            if(this._MatrixIsCreate(a1) && this._MatrixIsCreate(a2))
+                list[pos] = list[a1] * list[a2];
+            else
+            {
+                Console.Out.WriteLine("Matrix isn't create!!!");
+            }
+        }
+
+        private void _Trans()
+        {
+            Console.Out.WriteLine("Print number Matrix in list: ");
+            int a = this._ReaderInt();
+            Console.Out.WriteLine("Print position in list: ");
+            int pos = this._ReaderInt();
+            if(this._MatrixIsCreate(a))
+                list[pos] = list[a].Transposition();
+            else
+            {
+                Console.Out.WriteLine("Matrix isn't create!!!");
+            }
+        }
+
+        private void _PrintMatrix()
+        {
+            Console.Out.WriteLine("Print number Matrix in list: ");
+            int a = this._ReaderInt();
+            if (this._MatrixIsCreate(a))
+            {
+                for (int i = 0; i < list[a].Row; i++)
+                {
+                    string s = "";
+                    for (int j = 0; j < list[a].Colon; j++)
+                    {
+                        s += list[a].GetElement(i, j).ToString();
+                        s += " ";
+                    }
+                    Console.Out.WriteLine(s);
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine("Matrix isn't create!!!");
+            }
+        }
+
+        private int _ReaderInt()
+        {
+            int result = -1;
+            while (result == -1)
+            {
+                try
+                {
+                    string c = Console.In.ReadLine();
+                    result = int.Parse(c);
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine("Pardon, Try again!!!");
+                    result = -1;
+                }
+                
+            }
+            return result;
+        }
+
+        private Matrix _ReadMatrix(int h, int l)
+        {
+            Matrix m = new Matrix(h, l);
+            for(int i = 0; i < h; i++)
+            {
+                for(int j = 0; j < l; j++)
+                {
+                    int value = this._ReaderInt();
+                    m.SetElement(i, j, value);
+                }
+            }
+            return m;
+        }
+
+        private bool _MatrixIsCreate(int pos){return list[pos] != null;}
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace Laba1
+{
+    class Matrix
+    {
+
+        private int _colonCount;
+        private int _rowCount;
+
+        public int Colon { get { return this._colonCount; } }
+        public int Row { get { return this._rowCount; } }
+
+        public Matrix(int sizeX, int sizeY, int startValue = 0)
+        {
+            this._values = new int[sizeX][];
+            this._colonCount = sizeY;
+            this._rowCount = sizeX;
+            for (int i = 0; i < sizeX; i++)
+            {
+                this._values[i] = new int[sizeY];
+                for (int j = 0; j < sizeY; j++)
+                {
+                    this._values[i][j] = startValue;
+                }
+            }
+        }
+
+        public int GetElement(int colon, int row) { if (this._isParametrsInNormal(colon, row)) return this._values[colon][row]; else return -1; }
+        public void SetElement(int colon, int row, int value) { if (this._isParametrsInNormal(colon, row)) this._values[colon][row] = value; }
+
+        public Matrix Transposition()
+        {
+            Matrix result = new Matrix(this._colonCount, this._rowCount);
+
+            for(int i = 0; i < this._rowCount; i++)
+                 for(int j = 0; j < this._colonCount; j++)                
+                    result.SetElement(j, i, this.GetElement(i, j));                     
+
+            return result;
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if(Matrix._EqualSize(a, b))
+            {
+                Matrix result = new Matrix(a._rowCount, a._colonCount);
+                for(int i = 0; i < result._rowCount; i++)
+                {
+                    for(int j = 0; j < result._colonCount; j++)
+                    {
+                        result.SetElement(i, j, a.GetElement(i, j) + b.GetElement(i, j));
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                Matrix result = new Matrix(a._rowCount, a._colonCount);
+                return result;
+            }
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (Matrix._CanIMulti(a, b))
+            {
+                Matrix result = new Matrix(a._rowCount, b._colonCount);
+                for(int i = 0; i < a._rowCount; i++)
+                {
+                    for(int j = 0; j < b._colonCount; j++)
+                    {
+                        int newValue = 0;
+                        for( int k = 0; k < a._colonCount; k++)
+                        {
+                            newValue += a.GetElement(i, k) * b.GetElement(k, j);
+                        }
+                        result.SetElement(i, j, newValue);
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                Matrix result = new Matrix(a._rowCount, a._colonCount);
+                return result;
+            }
+        }
+
+        private int[][] _values;
+
+        private bool _isParametrsInNormal(int colon, int row) { return colon >= 0 && colon < this._colonCount && row >= 0 && row < this._rowCount; }
+    
+        private static bool _EqualSize(Matrix a, Matrix b) { return a._rowCount == b._rowCount && a._colonCount == b._colonCount;}
+        private static bool _CanIMulti(Matrix a, Matrix b) { return a._colonCount == b._rowCount; }
+    }
+}
